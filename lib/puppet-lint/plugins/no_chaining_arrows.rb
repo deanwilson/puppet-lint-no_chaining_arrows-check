8e901816b35aceff6c47a4dbca525d665a989a2c
Dean Wilson <dean.wilson@gmail.com>
2019-10-17 10:55:44 +0100

    Satisfy rubocop

--- lib/puppet-lint/plugins/no_chaining_arrows.rb
+++ lib/puppet-lint/plugins/no_chaining_arrows.rb
@@ -1,18 +1,17 @@
 PuppetLint.new_check(:no_chaining_arrows) do
-
   # stop puppet-lint raising its own arrow check
   # this way we don't get two warnings on <- and <~
   PuppetLint.configuration.send('disable_right_to_left_relationship')
 
   def check
-    arrow_tokens = [:OUT_EDGE, :OUT_EDGE_SUB, :IN_EDGE, :IN_EDGE_SUB]
+    arrow_tokens = %i[OUT_EDGE OUT_EDGE_SUB IN_EDGE IN_EDGE_SUB]
 
     tokens.select { |r| arrow_tokens.include? r.type }.each do |token|
       notify(
         :warning,
-        :message => 'chaining arrow found',
-        :line    => token.line,
-        :column  => token.column
+        message: 'chaining arrow found',
+        line: token.line,
+        column: token.column
       )
     end
   end
